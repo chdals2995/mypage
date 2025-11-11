6515ee9a48ab116d13f9115fa122d61ecb2e48d9
chdals2995 <chdals2995@gmail.com>
2025-11-11 19:08:53 +0900

    1111
    텍스트 패치 여러 경우 추가

--- reactStudy/src/component/study3/TestFetch.jsx
+++ reactStudy/src/component/study3/TestFetch.jsx
@@ -10,20 +10,77 @@ export default function TestFetch(){ // 패치는 비동기 동작으로 해야 
             .then( (response) => response.json() ) // 위의 값을 매개변수 respone에 받음(대문자x) res로 써야 함  json으로 변환
             .then( (data) => setUsers(data)); // 위의 값을 매개변수 data로 받음
 
-        },[]
+        },[] // fetch로 데이터 받아올 때 빈 배열 무조건! > 안 하면 계속 무한으로 값 가져옴.
     );
     return(
         <>
-        <h2>데이터 출력</h2>
-        <ul>
-            {
-                users.map( (u) => (
-                    <li>{u.name} : {u.email}</li>
-                )
-                )
-            }
-        </ul>
+            <h2>데이터 출력</h2>
+            <ul>
+                {
+                    users.map( (u) => (
+                        <li>{u.name} : {u.email}</li>
+                    )
+                    )
+                }
+            </ul>
 
         </>
     );
 }
+
+/* // map을 이용하지 않은 버전
+
+import { useEffect, useState } from "react";
+
+export default function TestFetch() {
+
+  const [city, setCity] = useState("");
+
+  useEffect(() => {
+    fetch("https://api.zippopotam.us/us/90210")
+      .then((res) => res.json())
+      .then((data) => {
+        setCity(data.places[0]["place name"]);
+      });
+  }, []);
+
+  return (
+    <>
+      <h2>도시 이름</h2>
+      <p>{city}</p>
+    </>
+  );
+}
+
+
+// map을 이용한 버전 
+
+import { useState, useEffect } from 'react';
+
+export default function PlaceFetch() {
+  const [places, setPlaces] = useState([]); // 배열로 저장!
+
+  useEffect(() => {
+    fetch("https://api.zippopotam.us/us/90210")
+      .then((res) => res.json())
+      .then((data) => {
+        setPlaces(data.places); // 배열 그대로 저장
+      });
+  }, []);
+
+  return (
+    <>
+      <h2>도시 이름 출력 (map 버전)</h2>
+      <ul>
+        {places.map((p, i) => (
+            // i = index 적용
+          <li key={i}>
+            도시 이름 : {p["place name"]}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+} */
+
+
